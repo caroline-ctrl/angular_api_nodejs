c9f6bc0e8e9678ddbb176d4116a17f72d0f52058
caroline-ctrl <duon.caroline@gmail.com>
2020-06-18 15:03:18 +0200

    methode et vue du getbyid

--- product/src/app/product/product-details/product-details.component.ts
+++ product/src/app/product/product-details/product-details.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ProductService } from '../product.service';
+import { ActivatedRoute, Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-product-details',
@@ -6,10 +9,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product-details.component.css']
 })
 export class ProductDetailsComponent implements OnInit {
+currentProduct = null;
 
-  constructor() { }
+  constructor(private productService: ProductService, private route: ActivatedRoute, private router: Router) { }
 
   ngOnInit(): void {
+    // a l'ouverture de la page, il lance la methode et 
+    // recupère dans l'url l'id du livre
+    this.getProductById(this.route.snapshot.paramMap.get('id'));
+  }
+
+  getProductById(id)
+  {
+    this.productService.getProductById(id).subscribe(product => {
+      this.currentProduct = product;
+    }, error => {
+      console.log(error);
+    });
+
   }
 
 }
